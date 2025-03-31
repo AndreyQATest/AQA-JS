7b100d1567451ca056d9bed1b5dad076006d349e
AndreyQA <marynichandrii@gmail.com>
2025-03-31 15:22:41 +0300

    jsBasis

---
+++ js-practice/jsBasis/task2.js
@@ -0,0 +1,8 @@
+// Оголошення змінних різних примітивних типів даних
+let name1 = "TestName "; 
+let name2 = "Андрій";
+let greetingTemplate = `Привіт, ${name1} та ${name2}!`;
+// Вивід значень у консоль
+console.log("ПРИВІТ:", name1+name2);
+console.log(greetingTemplate);
+
